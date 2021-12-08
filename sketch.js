7c8c4e18e73faa454784a995e449d826eee283c1
JarrylAncheta <55153273+JarrylAncheta@users.noreply.github.com>
2021-12-08 04:56:53 -0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,26 +1,53 @@
+var image;
+var song;
+var button;
 var x, y;
 var score = 0;
 const radius = 20;
 var r, g, b;
 
-function setup() {
+function setup(){
   createCanvas(windowWidth, windowHeight);
-  img= loadImage('Snow.jpg')
-  x = random(windowWidth);
-  y = random(windowHeight);
-  r = random(255);
-  g = random(255);
-  b = random(255);
+  song = loadSound('IntenseMusic.mp3');
+  button = createButton("play");
+  button.mousePressed(togglePlaying);
+  background(51);
+  button.style('font-size', '25px');
+  button.position(570,10);
 }
 
-function draw() {
-  background(220);
+function togglePlaying() {
+  if (!song.isPlaying()) {
+    song.play();
+    button.html("pause");
+} else {
+  song.pause();
+  button.html("play");
+}
+}
+
+function loaded() {
+  console.log("loaded");
+}
+
+
+function draw(){
+  imageMode(CENTER);
+  image(landscape, width/2, height/2, 750, 590);
   // Draw a circle
-  noStroke();
+   noStroke();
   fill(r, g, b);
-  ellipse(x, y, radius*3, radius*3);
+  ellipse(x, y, radius*1.5, radius*1.5);
   textSize(32);
   text("Score: " + score, 10, 30);
+  textSize(40);
+  stroke(82, 82, 82);
+  strokeWeight(2.5);
+  text("Catch the Snowball", 150, 120);
+}
+
+function preload(){
+  landscape = loadImage('Snow.jpg')
 }
 
 // When the user clicks the mouse
@@ -33,6 +60,7 @@ function mousePressed() {
   }
 }
 
+//Circle colorsl
 function newCircle() {
   x = random(windowWidth);
   y = random(windowHeight);
@@ -41,4 +69,5 @@ function newCircle() {
   b = random(255);
 }
 
-setInterval(newCircle, 850);
+//Determines how fast a new circle appears
+setInterval(newCircle, 900);
